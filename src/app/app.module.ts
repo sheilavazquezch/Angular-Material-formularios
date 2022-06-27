fdc70a3852715326563f5ea857f5c2947244f946
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-26 19:06:29 -0500

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
